5dcc519e32f884021aa6027c991bbc8ce30ee780
itZhy <zhy1987819@163.com>
2013-01-30 14:22:36 +0800

    deal with a critical situation when player forward over a lap.

--- src/main/java/Player/Position.java
+++ src/main/java/Player/Position.java
@@ -2,6 +2,8 @@ package Player;
 
 public class Position {
     private Integer index;
+    private final Integer maxIndex = 69;
+
 
     public Position(int index) {
         this.index = index;
@@ -9,6 +11,7 @@ public class Position {
 
     public void change(int step) {
         index += step;
+        index %= maxIndex + 1;
     }
 
     public boolean equals(Object position) {
--- src/test/java/Player/PlayerTest.java
+++ src/test/java/Player/PlayerTest.java
@@ -16,4 +16,15 @@ public class PlayerTest {
         //then
         assertThat(player, is(new Player(new Position(3))));
     }
+
+    @Test
+    public void its_position_should_be_1_after_forward_more_than_a_lap() {
+        //given
+        Position initialPosition = new Position(67);
+        Player player = new Player(initialPosition);
+        //when
+        player.forward(4);
+        //then
+        assertThat(player, is(new Player(new Position(1))));
+    }
 }
