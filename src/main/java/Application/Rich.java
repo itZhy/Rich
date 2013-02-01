9e347eebf4126f73811e351bb9cb3e7745797c49
itZhy <zhy1987819@163.com>
2013-02-01 13:14:25 +0800

    add rich class in Application package, it is the main class of this game.

---
+++ src/main/java/Application/Rich.java
@@ -0,0 +1,9 @@
+package Application;
+
+import Display.UI;
+
+public class Rich {
+    public static void main(String args[])  {
+        new UI().show();
+    }
+}
--- src/main/java/Display/UI.java
+++ src/main/java/Display/UI.java
@@ -6,11 +6,7 @@ import org.fusesource.jansi.AnsiConsole;
 import static org.fusesource.jansi.Ansi.ansi;
 
 public class UI {
-    public static void main(String args[]) {
-        new UI().show();
-    }
-
     public void show() {
-        AnsiConsole.out.println(ansi().eraseScreen().fg(Ansi.Color.RED).a("Hello").fg(Ansi.Color.GREEN).a(" World").reset());
+        AnsiConsole.out.println(ansi().fg(Ansi.Color.RED).a("Hello").fg(Ansi.Color.GREEN).a(" World").reset());
     }
 }
