48431c90a72d8772a311b67e2dea3986aecc0b7f
itZhy <zhy1987819@163.com>
2013-02-04 16:38:29 +0800

    add Command package and Roll class in it.

---
+++ src/test/java/Command/RollTest.java
@@ -0,0 +1,21 @@
+package Command;
+
+import Player.Position;
+import Player.Player;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class RollTest {
+    @Test
+    public void it_should_execute_roll_command_and_let_player_forward_5_steps () {
+        //given
+        Player player = new Player(new Position(0));
+        Command rollCommand = new Roll();
+        //when
+        rollCommand.execute(player);
+        //then
+        assertThat(player, is(new Player(new Position(5))));
+    }
+}
