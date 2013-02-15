ec63a608acf68bc1b25552e255d9151c505d8e90
itZhy <zhy1987819@163.com>
2013-02-16 07:31:37 +0800

    add deposit and withdraw function in PointsBank class.

--- src/test/java/Props/PointsBankTest.java
+++ src/test/java/Props/PointsBankTest.java
@@ -4,23 +4,48 @@ import Player.Position;
 import Player.Role;
 import Player.UncleTuu;
 import UI.Map;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class PointsBankTest {
+    private PointsBank pointsBank;
+    private Role uncleTuu;
+
+    @Before
+    public void setUp() {
+        pointsBank = new PointsBank();
+        uncleTuu = new UncleTuu(new Position(0), new Map());
+    }
+
     @Test
     public void uncle_tuu_should_has_15_points_after_added() {
-        //given
-        PointsBank pointsBank = new PointsBank();
-        Role uncleTuu = new UncleTuu(new Position(0), new Map());
         //when
-        pointsBank.add(uncleTuu, 5);
-        pointsBank.add(uncleTuu, 10);
+        pointsBank.deposit(uncleTuu, 5);
+        pointsBank.deposit(uncleTuu, 10);
         //then
         PointsBank expectedPointsBank = new PointsBank();
-        expectedPointsBank.add(uncleTuu, 15);
+        expectedPointsBank.deposit(uncleTuu, 15);
         assertThat(pointsBank, is(expectedPointsBank));
+    }
+
+    @Test
+    public void uncle_tuu_should_not_be_able_to_afford_a_prop_worth_30_points() {
+        //when
+        pointsBank.deposit(uncleTuu, 29);
+        boolean isSuccessful = pointsBank.withdraw(uncleTuu, 30);
+        //then
+        assertThat(isSuccessful, is(false));
+    }
+
+    @Test
+    public void uncle_tuu_should_be_able_to_afford_a_prop_worth_50_points() {
+        //when
+        pointsBank.deposit(uncleTuu, 50);
+        boolean isSuccessful = pointsBank.withdraw(uncleTuu, 50);
+        //then
+        assertThat(isSuccessful, is(true));
     }
 }
