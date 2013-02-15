032430b414f0e5338cc40bc45b70703ff0f90a8f
itZhy <zhy1987819@163.com>
2013-02-15 16:53:02 +0800

    when the content of position_to_surface_index isn't correct, throw an exception to quit the game.

--- src/main/java/Command/Quit.java
+++ src/main/java/Command/Quit.java
@@ -3,7 +3,6 @@ package Command;
 import Player.Player;
 import UI.UIException;
 
-
 public class Quit implements Command {
     public void execute(Player player) {
         throw new UIException("游戏被中止，正在退出...", UIException.NEED_QUIT);
