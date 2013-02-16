1d40d74e5cace193a23e5de1ed8b22caf65d3b56
itZhy <zhy1987819@163.com>
2013-02-16 11:11:52 +0800

    add stay and skip function in Role class.

--- src/main/java/Player/Role.java
+++ src/main/java/Player/Role.java
@@ -3,13 +3,15 @@ package Player;
 import UI.Element;
 
 public abstract class Role {
+    private static final Position HOSPITAL = new Position(14);
     private Position currentPosition;
+    private int remainTimes = 0;
 
     Role(Position initialPosition) {
         currentPosition = initialPosition;
     }
 
-    public String name()    {
+    public String name() {
         return getClass().toString();
     }
 
@@ -18,13 +20,33 @@ public abstract class Role {
         currentPosition = currentPosition.move(step);
     }
 
+    public void stay(int times) {
+        remainTimes = times;
+    }
+
+    public boolean skip() {
+        if (0 == remainTimes) {
+            return false;
+        }
+        --remainTimes;
+        return true;
+    }
+
+    public void moveToHospital() {
+        updateUI(currentPosition, HOSPITAL);
+        currentPosition = HOSPITAL;
+        stay(3);
+    }
+
     public boolean equals(Object object) {
         return getClass() == object.getClass() &&
                 currentPosition.equals(((Role) object).currentPosition);
     }
 
     public abstract String getPromptMessage();
+
     public abstract Element getElement(char symbol);
+
     protected abstract void updateUI(Position source, Position destination);
 
 }
--- src/main/java/Player/Rounder.java
+++ src/main/java/Player/Rounder.java
@@ -6,13 +6,16 @@ import java.util.List;
 public class Rounder {
     private final List<Role> orderedRoles = new ArrayList<Role>();
     private int currentIndex = 0;
+
     public Role current() {
         return orderedRoles.get(currentIndex);
     }
 
     public void next() {
-        ++currentIndex;
-        currentIndex %= orderedRoles.size();
+        do {
+            ++currentIndex;
+            currentIndex %= orderedRoles.size();
+        } while (orderedRoles.get(currentIndex).skip());
     }
 
     public void add(Role role) {
