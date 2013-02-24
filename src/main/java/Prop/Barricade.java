34f0b6a571840aeb816e0e6e4c0ac429be0d1fe4
itZhy <zhy1987819@163.com>
2013-02-24 11:23:46 +0800

    modify some display message.

--- src/main/java/Prop/Barricade.java
+++ src/main/java/Prop/Barricade.java
@@ -2,6 +2,7 @@ package Prop;
 
 import Player.Feature;
 import Player.Movement;
+import UI.CommandLine;
 
 public class Barricade extends Prop {
     public Barricade(String owner) {
@@ -12,5 +13,10 @@ public class Barricade extends Prop {
 
     public void handle(Movement movement) {
         movement.block(1);
+        showPromptMessage();
+    }
+
+    private void showPromptMessage() {
+        new CommandLine().outputInNewline("遭遇路障，停止前进。");
     }
 }
