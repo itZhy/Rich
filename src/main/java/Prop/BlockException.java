6acc0ba6fc505bec1fbb9b67329e874872bba03e
itZhy <zhy1987819@163.com>
2013-02-27 22:00:12 +0800

    remove unused file.

--- src/main/java/Prop/BlockException.java
+++
@@ -1,9 +0,0 @@
-package Prop;
-
-import UI.CommandLine;
-
-public class BlockException extends RuntimeException {
-    public void showPromptMessage() {
-        new CommandLine().outputInNewline("遭遇路障，停止前进。");
-    }
-}
