8eda254bc94844791fb3a52d45c0db5daa1bb38d
itZhy <zhy1987819@163.com>
2013-03-21 13:01:39 +0800

    refactor Interaction, change the while's position.

--- src/main/java/Application/Interaction.java
+++ src/main/java/Application/Interaction.java
@@ -7,36 +7,32 @@ public class Interaction {
     private Controller controller = null;
 
     public void run() {
-        initializeControllerUntilSucceed();
-        handleCommandUntilQuit();
+        initializeController();
+        setInitialFund();
+        handleCommand();
     }
 
-    private void initializeControllerUntilSucceed() {
-        while (initializeController()) {
-        }
-        while (setInitialFund()) {
-        }
-    }
-
-    private boolean initializeController() {
-        try {
-            controller = new Controller(commandLine.waitForInput(
-                    "请选择2~4位不重复玩家，输入编号即可。(1.钱夫人; 2.阿土伯; 3.孙小美; 4.金贝贝):"));
-            return false;
-        } catch (GameException e) {
-            commandLine.outputInNewline(e.toString());
-            return e.isNeedRetry();
+    private void initializeController() {
+        while (true) {
+            try {
+                controller = new Controller(commandLine.waitForInput(
+                        "请选择2~4位不重复玩家，输入编号即可。(1.钱夫人; 2.阿土伯; 3.孙小美; 4.金贝贝):"));
+                return;
+            } catch (GameException e) {
+                commandLine.outputInNewline(e.toString());
+            }
         }
     }
 
-    private boolean setInitialFund() {
-        try {
-            String input = commandLine.waitForInput("请输入玩家初始资金，范围1000～50000（默认10000）");
-            setInitialFundByInput(input);
-            return false;
-        } catch (GameException e) {
-            commandLine.outputInNewline(e.toString());
-            return e.isNeedRetry();
+    private void setInitialFund() {
+        while (true) {
+            try {
+                String input = commandLine.waitForInput("请输入玩家初始资金，范围1000～50000（默认10000）");
+                setInitialFundByInput(input);
+                return;
+            } catch (GameException e) {
+                commandLine.outputInNewline(e.toString());
+            }
         }
     }
 
@@ -47,19 +43,17 @@ public class Interaction {
         controller.initialFund(input);
     }
 
-    private void handleCommandUntilQuit() {
-        while (handleCommand()) {
-        }
-    }
-
-    private boolean handleCommand() {
-        try {
-            commandLine.output(controller.getPrompt());
-            controller.handleCommand(commandLine.waitForInput());
-            return true;
-        } catch (GameException e) {
-            commandLine.outputInNewline(e.toString());
-            return e.isNeedRetry();
+    private void handleCommand() {
+        while (true) {
+            try {
+                commandLine.output(controller.getPrompt());
+                controller.handleCommand(commandLine.waitForInput());
+            } catch (GameException e) {
+                commandLine.outputInNewline(e.toString());
+                if (!e.isNeedRetry()) {
+                    return;
+                }
+            }
         }
     }
 }
