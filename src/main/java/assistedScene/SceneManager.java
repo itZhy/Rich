03d11ddcd0625a638bfefe0d278f2026c261c243
itZhy <zhy1987819@163.com>
2013-03-31 14:32:34 +0800

    refactor SceneManager.

--- src/main/java/assistedScene/SceneManager.java
+++ src/main/java/assistedScene/SceneManager.java
@@ -13,17 +13,14 @@ import java.util.Map;
 
 public class SceneManager implements PlayerObserver {
     private final Map<Position, Scene> scenes = new HashMap<Position, Scene>();
+    private final PositionExtractor positionExtractor = new PositionExtractor();
 
     public SceneManager(PropManager propManager, EstateManager estateManager) {
-        initializeScenes(propManager, estateManager);
-    }
-
-    private void initializeScenes(PropManager propManager, EstateManager estateManager) {
         scenes.putAll(new PointPoolFactory(propManager).get());
-        scenes.put(new PositionExtractor().getMagicHouse(), new MagicHouse());
-        scenes.put(new PositionExtractor().getGiftHouse(), new GiftHouse(propManager, estateManager));
-        scenes.put(new PositionExtractor().getPropHouse(), new PropHouse(propManager));
-        scenes.put(new PositionExtractor().getPrison(), new Prison());
+        scenes.put(positionExtractor.getMagicHouse(), new MagicHouse());
+        scenes.put(positionExtractor.getGiftHouse(), new GiftHouse(propManager, estateManager));
+        scenes.put(positionExtractor.getPropHouse(), new PropHouse(propManager));
+        scenes.put(positionExtractor.getPrison(), new Prison());
     }
 
     public void handle(Role role, Movement movement) {
