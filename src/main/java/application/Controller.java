5444a9e411260e40c3dd7a7b4c16b3dc0c647606
itZhy <zhy1987819@163.com>
2013-03-31 13:30:03 +0800

    use Checker.check to replace checkPlayer function.

--- src/main/java/application/Controller.java
+++ src/main/java/application/Controller.java
@@ -7,6 +7,7 @@ import player.PlayerParser;
 import player.Rounder;
 import ui.Map;
 import ui.UIObserver;
+import utils.Checker;
 
 class Controller {
     private final Rounder rounder = new Rounder();
@@ -41,16 +42,10 @@ class Controller {
     }
 
     private void initializeRounder(String players) {
-        checkInputPlayers(players);
+        Checker.check(!players.isEmpty(), "您尚未选择任何玩家，请重新输入。");
         PlayerParser parser = new PlayerParser(subSystem.getObservers());
         for (int index = 0; index != players.length(); ++index) {
             rounder.add(parser.get(players.charAt(index)));
-        }
-    }
-
-    private void checkInputPlayers(String players) {
-        if (players.isEmpty()) {
-            throw new GameException("您尚未选择任何玩家，请重新输入。");
         }
     }
 }
